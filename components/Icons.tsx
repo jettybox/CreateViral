b0e56e26430b99143b03d7a9e830b52b986e42e9
jettybox <jettybox@gmail.com>
2025-11-09 21:35:25 +0000

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -89,3 +89,9 @@ export const SortAscendingIcon: React.FC<{ className?: string }> = ({ className 
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4h13M3 8h9M3 12h9m0 0l-3.5-3.5M12 12l-3.5 3.5M19 4v16m0 0l-3.5-3.5M19 20l3.5-3.5" />
     </svg>
 );
+
+export const WarningIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+    </svg>
+);
