5c6fa3db946fcfbbc47b12eb1726d3892b3f555d
jettybox <jettybox@gmail.com>
2025-11-10 19:50:35 +0000

    Update types.ts

--- types.ts
+++ types.ts
@@ -11,6 +11,7 @@ export interface VideoFile {
   height?: number;
   commercialAppeal: number; // Score from 1-100 predicted by AI
   isFeatured: boolean;     // Manually set by the admin
+  isFree: boolean;         // Manually set by the admin
   createdAt: number;       // Timestamp for "Newest" sorting
   generatedThumbnail?: string; // Base64 data URL for thumbnails generated on the fly
 }
